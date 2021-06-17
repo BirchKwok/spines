c609cb2cf38f2abcec32bb33a53af00765a629df
“birch” <768907597@qq.com>
2021-06-17 16:51:15 +0800

    Modify some functions.

--- spines/metrics.py
+++ spines/metrics.py
@@ -3,15 +3,18 @@ from . import *
 
 class RegModelEvaluate:
     """regression model evaluate functions."""
-
-    def r_square(self, y, y_hat):
+    @staticmethod
+    def r_square(y, y_hat):
         return 1 - (np.sum(np.power(y - y_hat, 2) )/ np.sum(np.power(y - np.mean(y), 2)))
 
-    def rmse(self, y, y_hat):
+    @staticmethod
+    def rmse(y, y_hat):
         return np.sqrt( 1 /len(y) * np.sum(y - y_hat) ** 2)
 
-    def mae(self, y, y_hat):
+    @staticmethod
+    def mae(y, y_hat):
         return 1 / len(y) * np.sum(np.abs( y -y_hat))
 
-    def mape(self, y, y_hat):
+    @staticmethod
+    def mape(y, y_hat):
         return 1/ len(y) * np.sum(np.abs((y - y_hat) / y))
