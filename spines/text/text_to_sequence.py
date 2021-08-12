48d28eb87a020d68803486d2d3478ebe08778df3
“birch” <BirchKwok@gmail.com>
2021-08-12 21:29:58 +0800

    Fixed BowTransformer bug and added inverse_transform functions.

--- spines/text/text_to_sequence.py
+++ spines/text/text_to_sequence.py
@@ -2,11 +2,14 @@ from collections import Counter
 from scipy import sparse as sp
 import re
 import numpy as np
+from typing import *
 
+from spines.text._stop_words_en import STOP_WORDS_EN
+from spines.text._stop_words_zh import STOP_WORDS_ZH
 
 __all__ = [
     'BoWTransformer',
-    'TfidfTransformer'
+    'TfidfVectorizer'
 ]
 
 
@@ -19,24 +22,27 @@ def _check_stop_list(stop):
         raise ValueError("not a built-in stop list: %s" % stop)
     elif stop is None:
         return None
-    else:  
+    else:
         return frozenset(stop)
+
 
 def _lower_en(doc):
     return doc.lower()
 
-def _re_pattern(pattern='(?u)\b\w\w+\b'):
+
+def _re_pattern(pattern='(?u)\b\w+\b'):
     return re.compile(pattern)
 
 
 class BoWTransformer:
     """Bag of words."""
+
     def __init__(self, lowercase=True, token_pattern=r"(?u)\b\w+\b",
-                stop_words=None, vocabulary=None, drop_oov=True):
+                 stop_words=None, vocabulary=None, drop_oov=True):
         self.lowercase = lowercase
         self._init_corpus = None
         self.uni_corpus_ = None
-        self.vocabulary = list(set(vocabulary)) if vocabulary is not None else vocabulary
+        self.vocabulary_ = list(set(vocabulary)) if vocabulary is not None else vocabulary
         self.get_corpus_idx_ = dict()
         self.get_corpus_freq_ = dict()
         self._token_pattern = _re_pattern(pattern=token_pattern)
@@ -44,13 +50,10 @@ class BoWTransformer:
         self.drop_oov = drop_oov
         self._corpus_sentence = None
 
-    
-        
     def _split_sentence(self):
         """split sentence to words"""
         return self._token_pattern.findall
-    
-    
+
     def _get_corpus_freq_dict(self, x):
         """
         count words frequence.
@@ -67,52 +70,50 @@ class BoWTransformer:
         """
         for i in x:
             self.get_corpus_idx_[i] = x.index(i)
-    
+
     def _get_doc_list(self, doc):
         """split words into list"""
-        
+
         if self.lowercase:
             doc = _lower_en(doc)
-            
-        spliter = self._split_sentence()
-        
-        
+
+        splitter = self._split_sentence()
+
         for i in doc:
             if '\u4e00' <= i <= '\u9fa5':  # if character is chinese
                 return doc
-            
-        return spliter(doc)
-         
+
+        return splitter(doc)
 
     def fit(self, corpus: list):
-        if self.vocabulary is None:
+        if self.vocabulary_ is None:
             self._init_corpus = []
             self._corpus_sentence = []
-            
+
             for i in corpus:
                 tmp = self._get_doc_list(i)
                 if isinstance(tmp, str):
                     self._init_corpus.append(tmp)
                 else:
                     self._init_corpus.extend(tmp)
-                
+
                 self._corpus_sentence.append(tmp)
-                
-            
-            self.uni_corpus_ = list(set(self._init_corpus)) 
-            self._get_corpus_freq_dict(self._init_corpus)    
+
+            self.uni_corpus_ = list(set(self._init_corpus))
+            self._get_corpus_freq_dict(self._init_corpus)
             self._get_corpus_idx(self.uni_corpus_)
         else:
-            self.uni_corpus_ = self.vocabulary 
-            self._get_corpus_freq_dict(corpus)    
+            self.uni_corpus_ = self.vocabulary_
+            self._get_corpus_freq_dict(corpus)
             self._get_corpus_idx(self.uni_corpus_)
 
         return self
 
-    
     def transform(self, corpus):
-        assert self.uni_corpus_ is not None and self.get_corpus_idx_ is not None and self.get_corpus_freq_ is not None, \
-        "Not fit yet."
+        assert self.uni_corpus_ is not None and self.get_corpus_idx_ is not None \
+               and self.get_corpus_freq_ is not None, \
+            "Not fit yet."
+
         c = []
         for i in corpus:
             tmp = self._get_doc_list(i)
@@ -121,20 +122,24 @@ class BoWTransformer:
             else:
                 c.extend(tmp)
 
-        
         indptr = [0]
-        
+
         indices = []
-        
-        num = np.sum([len(i) for i in self._corpus_sentence])
+
+        num = np.sum([len(i) if isinstance(i, Iterable) is True and isinstance(i, str) is False \
+                          else 1 for i in self._corpus_sentence])
+
         data = [self.get_corpus_freq_[c[i]] for i in range(num)]
 
         for d in self._corpus_sentence:
-            for term in d:
-                indices.append(self.get_corpus_idx_[term])
-                
-            indptr.append(len(indices))   
-        
+            if isinstance(d, Iterable) is True and isinstance(d, str) is False:
+                for term in d:
+                    indices.append(self.get_corpus_idx_[term])
+            else:
+                indices.append(self.get_corpus_idx_[d])
+
+            indptr.append(len(indices))
+
         X = sp.csr_matrix((data, indices, indptr))
 
         return X
@@ -142,4 +147,37 @@ class BoWTransformer:
     def fit_transform(self, corpus):
         self.fit(corpus)
         return self.transform(corpus)
-    
+
+    def inverse_transform(self, arr):
+        arr = np.asarray(arr)
+        arr_shape = arr.shape
+        res = []
+        for i in range(arr_shape[0]):
+            t = []
+            for j in range(len(arr[i])):
+                if arr[i][j] != 0:
+                    t.append(self.uni_corpus_[j])
+            res.append(t)
+
+        return res
+
+
+class TfidfVectorizer(BoWTransformer):
+    """TF-IDF transform from BoWTransformer"""
+    def __init__(self, smooth_idf=True,
+                                  ):
+        super(TfidfVectorizer, self).__init__()
+        self.smooth_idf = smooth_idf
+
+    def fit(self, corpus: list):
+        pass
+
+
+
+class TfidfTransformer:
+
+    def __init__(self):
+        pass
+
+
+
